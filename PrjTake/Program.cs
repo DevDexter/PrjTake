d2e53af02a91f844e5f166f9a71dffc3525d1ac9
DevDexter <DevDexter@hotmail.fi>
2012-09-01 22:31:17 +0200

    Added C# files
    More <3

---
+++ PrjTake/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using PrjTake.Utilities.Math;
+
+namespace PrjTake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Super Super = new Super();
+            Super.Init(Encoding.Default.GetBytes("PrjTake"));
+            Console.WriteLine(Super.Next());
+
+            while (true)
+                Console.Read();
+        }
+    }
+}
